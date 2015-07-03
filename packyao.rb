535b892e307aed96944f0b5d9c7a0a24c587b527
Ameir Abdeldayem <ameir.abdeldayem@teamaol.com>
2015-07-02 20:50:21 -0400

    Update to support new packyao changes.

--- packyao.rb
+++ packyao.rb
@@ -3,6 +3,8 @@ require 'json'
 version = '0.4.1'
 
 a = {}
+a['name'] = 'twemproxy';
+a['version'] = version;
 a['source'] = 'https://drive.google.com/uc?id=0B6pVMMV5F5dfb1YwcThnaVZXbjg&export=download';
 a['type'] = 'http';
 a['license']
@@ -10,17 +12,18 @@ a['commands'] = [
     'pwd',
     'apt-get -y install build-essential autoconf',
     'tar xvfz source',
-    'cd nutcracker-' + version + ' && CFLAGS="-ggdb3 -O0" ./configure --enable-debug=full && make && make install'
+    'umask 022; cd nutcracker-' + version + ' && CFLAGS="-ggdb3 -O0" ./configure --enable-debug=full && make'
 ];
 a['depends'] = [];
 a['cwd'] = '';
 a['env'] = [];
-a['version'] = version;
 a['output'] = 'deb';
-a['package_files'] = [
-    'nutcracker-' + version + '/src/nutcracker' => '/usr/local/sbin/nutcracker',
-    'nutcracker-' + version + '/man/nutcracker.8' => '/usr/local/share/man/man8/nutcracker.8'
-];
+a['package_files'] = {
+  'nutcracker-' + version + '/scripts/nutcracker.init' => '/etc/init.d/nutcracker',
+  'nutcracker-' + version + '/conf/nutcracker.yml' => '/etc/nutcracker/nutcracker.yml',
+  'nutcracker-' + version + '/src/nutcracker' => '/usr/local/sbin/nutcracker',
+  'nutcracker-' + version + '/man/nutcracker.8' => '/usr/local/share/man/man8/nutcracker.8'
+};
 a['build_distro'] = 'ubuntu';
 a['build_distro_version'] = '14.04';
 
